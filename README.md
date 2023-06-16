40e67d79907239668a3699c5105f7d5ee19fe4bf
ellehailu <ehailu16@georgefox.edu>
2023-06-16 11:04:26 -0700

    add a calculate price method and return the price of pizza based on the size

--- README.md
+++ README.md
@@ -2,5 +2,11 @@ Describe Pizza()
 Test: "It should return a pizza object with two properties for toppings and size"
 Code: const myPizza = new Pizza(["anchovies", "pineapple"], "medium");
 myPizza.toppings;
-Expected Output: Pizza { toppings: ["anchovies", "pineapple"], size: "medium" }
-['anchovies, pineapple']
+Expected Output: Pizza { toppings: ["anchovies", "pineapple"], size: "medium" };
+['anchovies, pineapple'];
+
+Test: "It should return a price for the pizza based on the selection of toppings and size the user has made"
+Code: const myPizza = new Pizza(["anchovies", "pineapple"], "medium");
+myPizza.calculatePrice();
+Expected Output: Pizza {toppings: ["anchovies", "pineapple"], size: "medium"};
+15;
--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,9 @@ function Pizza(toppings, size){
 
 //prototype.addToppings()
 //takes user input of toppings selected
+// Pizza.prototype.addToppings(topping) = function(){
 
+// }
 
 //prototype.selectSize()
 //takes user input for size
@@ -17,6 +19,20 @@ function Pizza(toppings, size){
 
 //prototype.calculate total price
 //takes number of toppings and size selected and returns a total price.
+Pizza.prototype.calculatePrice = function(){
+    let pizzaPrice = 0;
+    if (this.size === "small"){
+        pizzaPrice = 10;
+        
+    }
+    else if(this.size === "medium"){
+        pizzaPrice = 15;
+    }
+    else if(this.size === "large"){
+        pizzaPrice = 20;
+    }
+    return pizzaPrice;
+}
 
 
 
