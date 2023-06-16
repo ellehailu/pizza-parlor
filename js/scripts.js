c23e8df1d07df2f92916bbdfa0fe458c95de0b1c
ellehailu <ehailu16@georgefox.edu>
2023-06-16 10:28:06 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// Business Logic
+
+//Create a Pizza constructor 
+//Pizza (toppings, size)
+
+//prototype.addToppings()
+//takes user input of toppings selected
+
+//prototype.selectSize()
+//takes user input for size
+
+
+//prototype.calculate total price
+//takes number of toppings and size selected and returns a total price.
+
+
+
+
+// UI Logic
