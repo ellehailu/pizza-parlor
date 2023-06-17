04002cd912e7b1f55d46486528e3ede995542823
ellehailu <ehailu16@georgefox.edu>
2023-06-16 17:13:45 -0700

    add more UI functionality and print out the price for the pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,13 @@
 // Business Logic
-
-//Create a Pizza constructor 
-//Pizza (toppings, size)
-function Pizza(toppings, size){
+function Pizza(toppings, size) {
     this.toppings = toppings;
     this.size = size;
 }
 
-//prototype.addToppings()
-//takes user input of toppings selected
-// Pizza.prototype.addToppings(topping) = function(){
-
-// }
-
-//prototype.selectSize()
-//takes user input for size
-
-
-//prototype.calculate total price
-//takes number of toppings and size selected and returns a total price.
-//Price calculation: enter toppings into an array
-//if toppingsArray.length === 3 then price = 3
-//if toppingsArray.length === 2 then price = 2
-//if toppingsArray.length === 1 then price = 1
-Pizza.prototype.calculatePrice = function(){
+Pizza.prototype.calculatePrice = function() {
+    let pizzaPrice = 0;
     if (this.size === "small"){
         pizzaPrice = 10;
-        
     }
     else if(this.size === "medium"){
         pizzaPrice = 15;
@@ -38,19 +19,25 @@ Pizza.prototype.calculatePrice = function(){
 }
 
 // UI Logic
-function handleForm(event){
-    event.preventDefault();
-    const toppings = document.getElementsByName("toppings");
-    const size = document.getElementById("size");
-    let selectedToppings = [];
-    for(let i = 0; i < toppings.length; i++){
-        if(toppings[i].checked){
-            selectedToppings.push(toppings[i].value);
-        }
-    }
-    return selectedToppings;
+let myPizza = new Pizza();
+
+function showPrice(priceToDisplay){
+    let priceDiv = document.querySelector("div#totalCost");
+    priceDiv.innerText = null;
+    const p = document.createElement("p");
+    const cost = priceToDisplay.calculatePrice();
+    p.append(cost);
+    priceDiv.append(p);
 }
 
+function handleForm(event){
+    event.preventDefault();
+    const selectedToppings = document.getElementsByName("topping");
+    const selectedSize = document.querySelector("input[name='size']:checked").value;
+    myPizza = new Pizza(selectedToppings, selectedSize);
+    showPrice(myPizza);
+    
+}
 window.addEventListener("load", function(){
     document.getElementById("customOrder").addEventListener("submit", handleForm);
 })
